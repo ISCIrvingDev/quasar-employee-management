4c49210cb1c8f4ff78020971e2760b25c04d1052
Irving Salazar <ISCIrvingDev@gmail.com>
2025-02-11 14:55:26 -0600

    Add estensions to NewDepartmentDto

---
+++ src/application/api/services/dtos/extensions/new-department-dto.extension.ts
@@ -0,0 +1,17 @@
+import type { GetDepartmentDto } from '../department.dtos'
+import type { Record } from 'src/pages/deparment/deparment.models'
+
+export const NewDepartmentDtoExtensions = {
+  toRecord(getDepartmentDto: GetDepartmentDto): Record {
+    const res: Record = {
+      id: getDepartmentDto.id,
+      createdAt: getDepartmentDto.createdAt.toString(),
+      updatedAt: getDepartmentDto.updatedAt.toString(),
+      key: getDepartmentDto.key,
+      name: getDepartmentDto.name,
+      description: getDepartmentDto.description,
+    }
+
+    return res
+  },
+}
